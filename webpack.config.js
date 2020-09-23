13e3d0a0d69778c954267d4dc28b0a6a57531593
moaikang <kk_w1207@naver.com>
2020-09-23 13:22:16 +0900

    [ADD] : babel config
    바벨 설정을 추가하였다.

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,11 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader', // 바벨 로더를 추가한다
+      },
     ],
   },
   plugins: [
